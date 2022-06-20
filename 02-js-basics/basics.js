d7abae2069bacc7b2eaec70dfc160763907e7398
Michaela Bačíková <bacikova.michaela@gmail.com>
2022-06-20 10:22:13 +0200

    introduction, basics

---
+++ 02-js-basics/basics.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let a = 4;
+const b = 5;
+let c; //= undefined
+let d = null;
+console.log(c, d);
+
+
+//...
+let j = 5;
+let s1 = 'ab\"\'c';
+let s2 = "abc";
+let s3 = `
+  <h1>${s1}</h1>
+  <button>Click me!</button>
+  <div>
+    Toto je divko hehe.
+  </div>
+  `;
