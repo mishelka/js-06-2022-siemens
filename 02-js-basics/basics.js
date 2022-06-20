a10322c55b3f4c37018855a6dbfdf9ed5bd5f09b
Michaela Bačíková <bacikova.michaela@gmail.com>
2022-06-20 13:28:49 +0200

    basics finished

--- 02-js-basics/basics.js
+++ 02-js-basics/basics.js
@@ -8,7 +8,7 @@ console.log(c, d);
 
 
 //...
-let j = 5;
+let x = 5;
 let s1 = 'ab\"\'c';
 let s2 = "abc";
 let s3 = `
@@ -73,3 +73,62 @@ console.log(array[104].firstName);
 //console.log(array.104.firstname); //no!
 console.log(array['abc']);
 console.log(array.abc); //lebo index abc je string
+
+let i = 4; //5
+let j = 3; //2
+// i++; //i, i = i + 1;
+// ++i; //i = i + 1; i
+
+// console.log(i++ + ++j); //8
+   //j + (4   + 5 - 3   - 2)
+j += i++ + i - j-- - j; //7
+console.log(j);
+
+const t = true, f = false, tt = true, ff = false;
+
+console.log(!(t || tt));
+
+console.log(j < 6 ? 'large' : 'small');
+
+console.log(typeof j, typeof person, typeof array, typeof t, typeof s3);
+
+const theDay = 2;
+switch (theDay) {
+  case 1: case 2:
+    document.write("Again work!!!"); break;
+  case 5:
+    document.write("Finally Friday"); break;
+  case 6:
+    document.write("Super Saturday"); break;
+  case 0:
+    document.write("Sleepy Sunday"); break;
+  default:
+    document.write("I'm looking forward to this weekend!");
+}
+
+i = 0; //3
+while (i < 3) { //2? 3?
+  document.write("*"); //***
+  i++;
+}
+
+i = 0; //4
+do { //4?
+  document.write("*"); //****
+} while (i++ < 3);
+
+       //6
+for (let i = 0; i < 5; i = i + 2) { //2? 3?
+  document.write("*"); //***
+}
+
+console.log(">>>>BEGIN FOR OF");
+console.log(array);
+for(let v of array) {
+  console.log(v);
+}
+console.log(">>>>BEGIN FOR IN");
+for(let i in array) {
+  console.log(`[${i}]: ${array[i]}`);
+}
+console.log(">>>>END");
