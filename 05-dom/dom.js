4f3e075a06f45eb7e6b687c05a411fa14e4bd4d4
Michaela Bačíková <bacikova.michaela@gmail.com>
2022-06-22 13:23:45 +0200

    label for

--- 05-dom/dom.js
+++ 05-dom/dom.js
@@ -1,23 +1,8 @@
-const str = "TOto je nejaky dlhy retazec";
-console.log(str.substring(5, str.length));
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-console.log(str.split(' '));
-console.log(str.includes('k') || str.includes('K'));
+class Person {
+  constructor(name, birthDate, favouriteColor) {
+    this.name = name;
+    this.birthDate = birthDate;
+    this.favouriteColor = favouriteColor;
+  }
+}
 
-const d1 = new Date();
-const d2 = new Date(2015, 4, 12);
-console.log(d1.getTime() < d2.getTime());
-
-const reg = new RegExp("^(?<prva>ab+)(?<druha>c)$");
-const result1 = reg.exec("abbbbbbc");
-const result2 = reg.exec("ac");
-console.log(result1);
-
-console.log(result1[1]);
-
-let abc;
-
-setTimeout(() => { abc = 10; console.log("SET") }, 1000);
-
-console.log(abc);
