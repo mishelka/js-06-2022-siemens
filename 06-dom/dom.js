8a16d66f88a7f0081b2ceaef083d88bd42defb84
Michaela Bačíková <bacikova.michaela@gmail.com>
2022-06-23 12:57:47 +0200

    selected index, hide/show

--- 06-dom/dom.js
+++ 06-dom/dom.js
@@ -1,17 +1,19 @@
 class Person {
-  constructor(name, birthDate = new Date(), favouriteColor) {
+  constructor(id, name, birthDate = new Date(), favouriteColor) {
+    this.id = id;
     this.name = name;
     this.birthDate = birthDate;
     this.favouriteColor = favouriteColor;
   }
 }
 
-const persons = [
-  new Person("Jan", new Date(), "red"),
-  new Person("Michal", new Date(), "blue"),
-  new Person("Petra", new Date(), "red"),
-  new Person("Marcela", new Date(), "blue"),
+let persons = [
+  new Person(15, "Jan", new Date(), "red"),
+  new Person(25,"Michal", new Date(), "blue"),
+  new Person(0, "Petra", new Date(), "red"),
+  new Person(55, "Marcela", new Date(), "blue"),
 ];
+let selectedPersonId = -1;
 
 window.onload = () => renderTable();
 
@@ -19,6 +21,7 @@ function handleSubmit(event) {
   event.preventDefault();
   //validation!!
   const person = new Person(
+    persons.length + 1,
     document.personForm.fname.value,
     new Date(document.personForm.birthDate.value)
   );
@@ -38,15 +41,40 @@ function renderTable() {
   const table = document.getElementById("personTable")
   table.innerHTML = "";
   persons.forEach(p => table.innerHTML += `
-  <tr>
+  <tr id="person-row-${p.id}" 
+      onclick="selectRow(${p.id})"
+      class="${selectedPersonId === p.id ? 'selected' : ''}">
+    <td>${p.id}</td>
     <td>${p.name}</td>
     <td>${p.birthDate.toLocaleDateString()}</td>
   </tr>
   `);
 }
 
+function deletePerson() {
+  if(selectedPersonId !== -1) {
+    persons = persons.filter(p => p.id !== selectedPersonId);
+  }
+  selectedPersonId = -1;
+  renderTable();
+}
+
+function selectRow(p) {
+  console.log('selecting: ' + p);
+  selectedPersonId = p;
+  renderTable();
+}
+
 function moveLi() {
   let superWidget = document.getElementById("widget1");
   let ul = superWidget.parentNode;
   ul.appendChild(superWidget);
+}
+
+function hideLi() {
+  event.stopPropagation()
+  let ul = document.getElementById("ul");
+  const isHidden = ul.getAttribute("class")?.includes("hidden");
+  console.log(isHidden);
+  ul.setAttribute("class", isHidden ? "" : "hidden");
 }
