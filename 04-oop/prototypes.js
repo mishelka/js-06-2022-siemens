9fe483f38b6f2f394ccdcc573f0ad14899914d39
Michaela Bačíková <bacikova.michaela@gmail.com>
2022-06-24 13:31:10 +0200

    old prototypal JS OOP - dedenie metody

--- 04-oop/prototypes.js
+++ 04-oop/prototypes.js
@@ -23,10 +23,10 @@ console.log(genericEmployee.describe());
 
 //let's create a few more objects from the prototype
 const p1 = Object.create(genericPerson);
-p1.name = 'Peter';
+p1.name = 'p1 - Peter';
 p1.age = 25;
 const e1 = Object.create(genericEmployee);
-e1.name = 'Maria';
+e1.name = 'e1 - Maria';
 e1.age = 19;
 e1.jobTitle = 'frontend developer';
 
@@ -34,29 +34,26 @@ e1.jobTitle = 'frontend developer';
 function Person(name, age) {
   this.name = name;
   this.age = age;
-  // this.describe = function() {
-  //   return this.name + ", " + this.age;
-  // };
 }
+
+const p2 = new Person('p2 - Marian', 20);
+
 Person.prototype.describe = function() {
-    return this.name + ", " + this.age;
-};
-const p2 = new Person('Marian', 20);
+  return this.name + ", " + this.age;
+}
 
 //inheritance
 function Employee(name, age, jobTitle) {
-  Person.call(name, age);
+  Person.call(this, name, age);
   this.jobTitle = jobTitle;
-  // this.describe = function() {
-  //   return Person.prototype.describe() + ", " + jobTitle;
-  // }
 }
 Employee.prototype = Object.create(Person.prototype); //this is actual inheritance
 Employee.prototype.constructor = Employee; //constructor needs to be attached to the prototype
 Employee.prototype.describe = function() {
-  return  Object.getPrototypeOf(Object.getPrototypeOf(this)).describe() + ", " + this.jobTitle;
+  const str = Object.getPrototypeOf(Object.getPrototypeOf(this)).describe.call(this);
+  return str + ", " + this.jobTitle;
 }
-const e2 = new Employee('Gabika', 75, 'senior software architect');
+const e2 = new Employee('e2 - Gabika', 75, 'senior software architect');
 
 const persons = [p1, e1, p2, e2];
 persons.forEach(p => console.log(p));
